77d577ec6502314ea433d87fec8d5a31bbc1af57
Daniel Kirwan <danielkirwan07@gmail.com>
2021-09-03 10:45:30 +0100

    19

--- cavai.js
+++ cavai.js
@@ -4,7 +4,7 @@ setTimeout(function(){
         var s = target.document.createElement('script');
         s.type = 'text/javascript';
         s.id = 'cavai-script';
-        var theUrl = 'https://bannerboyz.cavai.com/js/converses/widgets/converse-widget.089cd4c67af73e8fdf103c8b4fe59c17.min.js?bust=' + Date.now();
+        var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1627298841620-1627288268379-1626873718672-1621429862720-1617810558896-23fbff718ad66343089e7b871fec1c6e.min.js?bust=' + Date.now();
         s.src = theUrl;
         s.async = true;
        
@@ -43,11 +43,7 @@ var clickFunction = function() {
     event.stopPropagation();
     return false;
 }
-
-
-setTimeout(function(){
-    target.addEventListener('click', clickFunction, true);
+target.addEventListener('click', clickFunction, true);
 target.addEventListener('touchstart', clickFunction, true);
-},2000);
 
-}, 1000);
+}, 500);
